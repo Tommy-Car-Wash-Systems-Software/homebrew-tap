be82515579686493539f4370d5398fa1a4e128d6
brianp-tommycarwash <111786142+brianp-tommycarwash@users.noreply.github.com>
2026-09-16 13:38:45 -0500

    Cask: Homebrew 7 DSL (symbol macos dep, no postflight)

--- Casks/stretch-goal.rb
+++ Casks/stretch-goal.rb
@@ -7,14 +7,9 @@ cask "stretch-goal" do
   desc "Menu bar nudges to move, stretch and hydrate, with a team leaderboard"
   homepage "https://github.com/Tommy-Car-Wash-Systems-Software/stretch-goal"
 
-  depends_on macos: ">= :tahoe"
+  depends_on macos: :tahoe
 
   app "Stretch Goal.app"
-
-  postflight do
-    # Ad-hoc signed, not notarized: without --no-quarantine Gatekeeper blocks first launch.
-    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{appdir}/Stretch Goal.app"], sudo: false
-  end
 
   zap trash: [
     "~/Library/Application Support/Stretch Goal",
