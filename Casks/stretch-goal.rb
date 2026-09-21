c3f1e40e22ea2965ef9b516e0bf2ea8915741831
brianp-tommycarwash <111786142+brianp-tommycarwash@users.noreply.github.com>
2026-09-21 14:28:06 -0500

    stretch-goal 0.6.0

--- Casks/stretch-goal.rb
+++ Casks/stretch-goal.rb
@@ -1,6 +1,6 @@
 cask "stretch-goal" do
-  version "0.5.1"
-  sha256 "9ea0a7ff788db076cc14f8b390ca7d87fa51da5e33aa27765e0df917bdd3d35d"
+  version "0.6.0"
+  sha256 "5ef4617daf3dc58010a149bc53d44b294809878780636a1eceee79377be9e321"
 
   url "https://github.com/Tommy-Car-Wash-Systems-Software/stretch-goal/releases/download/v#{version}/StretchGoal-#{version}.zip"
   name "Stretch Goal"
